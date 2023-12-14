41dd1bfec695fe8ef0f35d2d0cab1404e256a763
hoonjo123 <johoon030@gmail.com>
2023-12-14 09:01:55 +0900

    add

--- src/javapractice.java
+++ src/javapractice.java
@@ -5,28 +5,78 @@ import java.util.Map;
 
 public class javapractice {
     public static void main(String[] args) {
-        List<String> myList = new ArrayList<>();
-        myList.add("basketball");
-        myList.add("basketball");
-        myList.add("basketball");
-        myList.add("baseball");
-        myList.add("baseball");
-        myList.add("baseball");
-        myList.add("tennis");
-        myList.add("tennis");
-        myList.add("tennis");
+//        List<String> myList = new ArrayList<>();
+//        myList.add("basketball");
+//        myList.add("basketball");
+//        myList.add("basketball");
+//        myList.add("baseball");
+//        myList.add("baseball");
+//        myList.add("baseball");
+//        myList.add("tennis");
+//        myList.add("tennis");
+//        myList.add("tennis");
+//
+//        System.out.println(myList);
+//
+//        Map<String, Integer> mymap = new HashMap<>();
+//        for(String a : myList) {
+//            if(mymap.containsKey(a)) {
+//                mymap.put(a, mymap.get(a) + 1);
+//            }else{
+//                mymap.put(a, 1);
+//            }
+//            mymap.put(a,mymap.getOrDefault(a,0)+1);
+//        }
+//        System.out.println();
+//
+//        String[] my_string = {"He11oWor1d"};
+//        String[] overwrite_string = {"lloWorl"};
+//
+//        String str1 = "aaaaa";
+//        String str2 = "bbbbb";
+//        String answer = "";
+//        for(int i=0; i<str1.length(); i++){
+//            answer += str1.substring(i,i+1);
+//            answer += str2.substring(i,i+1);
+//        }
+//        System.out.println(answer); //[ababababab]
+//
+//        String[] arr = {"a","b","c"};
+//        String answer = "";
+//
+//        for(int i=0; i< arr.length; i++){
+//            answer += arr[i];
+//        }
+//        System.out.println(answer);
 
-        System.out.println(myList);
+//        String my_string = "string";
+//        String answer = "";
+//        int k = 3;
+//        for(int i = 0; i<k; i++){
+//            answer += my_string;
+//        }
+//        System.out.println(answer);
+//
+//        int a = 9;
+//        int b = 91;
+//
+//        //result = 991;
+//        //1. a와 b를 합친것과 b와 a를 합친걸 더한다.
+//        //2. int를 String 으로 변환해준다.
+//        String ab = String.valueOf(a) + String.valueOf(b);
+//        String ba = String.valueOf(b) + String.valueOf(a);
+//
+//        if(Integer.parseInt(ab) >= Integer.parseInt(ba)) {
+//            System.out.println(Integer.parseInt(ab));
+//        }else{
+//            System.out.println(Integer.parseInt(ba));
+//        }
 
-        Map<String, Integer> mymap = new HashMap<>();
-        for(String a : myList) {
-            if(mymap.containsKey(a)) {
-                mymap.put(a, mymap.get(a) + 1);
-            }else{
-                mymap.put(a, 1);
-            }
-            mymap.put(a,mymap.getOrDefault(a,0)+1);
-        }
-        System.out.println();
+        int a = 2;
+        int b = 91;
+        //result = 364;
+        String ab = String.valueOf(a) + String.valueOf(b);
+
+
     }
 }
