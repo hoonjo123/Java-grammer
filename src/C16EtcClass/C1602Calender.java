1a280b8090c2436b7ff8b0b99c0c97896158d0cc
hoonjo123 <johoon030@gmail.com>
2023-12-22 17:44:06 +0900

    add

--- src/C16EtcClass/C1602Calender.java
+++ src/C16EtcClass/C1602Calender.java
@@ -2,7 +2,9 @@ package C16EtcClass;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class C1602Calender {
     public static void main(String[] args) {
@@ -15,6 +17,29 @@ public class C1602Calender {
         System.out.println(time.getTime());
         //Fri Dec 22 17:28:44 KST 2023
 
+
+        // 특정한 숫자값은 get함수의 input값으로 주어 원하는 날짜 정보를 출력할 수 있게 해준다.
+        System.out.println(time.get(1)); //년도
+        System.out.println(time.get(2)+1); //전산상 월 //외우기가힘들군
+
+        System.out.println(time.get(Calendar.MONTH)+1);
+        System.out.println(time.get(Calendar.YEAR));
+        System.out.println(time.get(Calendar.DAY_OF_MONTH)+"일");
+        System.out.println(time.get(Calendar.DAY_OF_WEEK)+"요일"); //요일
+        System.out.println(time.get(Calendar.HOUR_OF_DAY)+"시"); //17시
+        System.out.println(time.get(Calendar.MINUTE)+"분"); //36
+        System.out.println(time.get(Calendar.SECOND)+"초"); //25
+
+
+        //Java.time 패키지 : local~ 클래스
+        LocalTime presentTime = LocalTime.now();
+        System.out.println(presentTime); // 17:41:02.405
+
+        LocalDate presentDate = LocalDate.now();
+        System.out.println(presentDate); //2023-12-22
+
+        LocalDateTime thisTime = LocalDateTime.now();
+        System.out.println(thisTime); //2023-12-22T17:42:18.167
 
     }
 }
